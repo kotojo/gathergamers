08f56c63a7ac2e9b055287e9f7ae7cd99e4855f9
Bruce Kellerman <brucek17@hotmail.com>
2018-08-06 20:50:28 -0400

    changed port

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('The start of something great\n');
-}).listen(8080);
+}).listen(3000);
 console.log('Server running at localhost:3000');
